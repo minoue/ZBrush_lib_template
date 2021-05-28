fae33f03ddda6949016cb3ea795acdf100788ff4
minoue <michitaka.inoue@icloud.com>
2021-05-28 09:54:41 +0100

    reformat

--- ZBrush_lib_template.hpp
+++ ZBrush_lib_template.hpp
@@ -8,23 +8,21 @@
 #include <CoreFoundation/CoreFoundation.h>
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
 
 //ZBrush expects these arguments for a dll function, though it's best to only use the first three
 float Version(char* pDontCare, double optValue, char* pOptBuffer1, int optBuffer1Size,
-              char* pOptBuffer2, int optBuffer2Size, char** zData);
-                            
+    char* pOptBuffer2, int optBuffer2Size, char** zData);
+
 float HelloLib(char* textFromZBrush, double valueFromZBrush, char* pOptBuffer1, int optBuffer1Size,
-	           char* pOptBuffer2, int optBuffer2Size, char** zData);
+    char* pOptBuffer2, int optBuffer2Size, char** zData);
 
 float SaveMemStringToFile(char* textFromZBrush, double valueFromZBrush, char* pOptBuffer1, int optBuffer1Size,
-	           char* pOptBuffer2, int optBuffer2Size, char** zData);
+    char* pOptBuffer2, int optBuffer2Size, char** zData);
 
 #ifdef __cplusplus
 }
 #endif
-
 
 #endif /* libTemplate_hpp */
